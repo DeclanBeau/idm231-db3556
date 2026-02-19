2cb525efd864ef7b2e7c4e56ddf760a7e49b6967
DeclanBeau <db3556@drexel.edu>
2026-02-19 16:34:52 -0500

    HTML and CSS mostly done, began JS work

---
+++ main.js
@@ -0,0 +1,12 @@
+const aires = document.getElementById('Aires');
+const taurus = document.getElementById('Taurus');
+const gemini = document.getElementById('Gemini');
+const cancer = document.getElementById('Cancer');
+const leo = document.getElementById('Leo');
+const virgo = document.getElementById('Virgo');
+const libra = document.getElementById('Libra');
+const scorpio = document.getElementById('Scorpio');
+const sagit = document.getElementById('Sagittarius')
+const capricorn = document.getElementById('Capricorn');
+const aqua = document.getElementById('Aquarius');
+
